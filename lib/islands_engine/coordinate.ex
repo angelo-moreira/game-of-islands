bbc17ca9df7e63ca85ae419ed3d09aa2f0dda04c
Angelo Moreira <angelo.m@designone.co.uk>
2018-08-09 07:39:07 +0100

    fix sintax error

--- lib/islands_engine/coordinate.ex
+++ lib/islands_engine/coordinate.ex
@@ -1,8 +1,11 @@
-defmodule do
+defmodule IslandsEngine.Coordinate do
   alias __MODULE__
 
   @enforce_keys [:row, :col]
-  defstruct [:row, :col:]
+  defstruct [:row, :col]
 
-  def new(row, col), do: {:ok, %Coordinate{row: row, col: col}}
+  def new(row, col) when row in @board_range and col in @board_range,
+    do: {:ok, %Coordinate{row: row, col: col}}
+
+  def new(_row, _col), do: {:error, :invalid_coordinate}
 end
